ae298909009838236b8b280bb27e876282e1d4f7
Hiroki Terashima <honchikun@gmail.com>
2018-01-08 11:20:41 -0800

    Updated regex to match new trial id format. Added highchart dependency.

---
+++ temperatureChangeTable.js
@@ -0,0 +1,286 @@
+let temperatures = ['Hot', 'Warm', 'Cold'];
+let materials = ['Styrofoam', 'Wood', 'Clay', 'Plastic', 'Glass', 'Aluminum'];
+
+var chart = null;
+var categories = [];
+var series = [];
+
+function init() {
+  //renderChart(categories, series);
+}
+
+function renderChart(categories, series, lowerRangePoint, upperRangePoint) {
+  let title = 'Change in temperature from ' + lowerRangePoint + ' min to ' + upperRangePoint + ' min';
+  chart = new Highcharts.chart('container', {
+      chart: {
+          type: 'column'
+      },
+      title: {
+          text: title
+      },
+      xAxis: {
+          categories: categories
+      },
+      yAxis: {
+          min: -26,
+          max: 26,
+          title: {
+              text: 'Change in Temperature'
+          }
+      },
+      credits: {
+          enabled: false
+      },
+      series: series
+  });
+}
+
+function sortTemperatures(tempA, tempB) {
+  let tempAIndex = temperatures.indexOf(tempA);
+  let tempBIndex = temperatures.indexOf(tempB);
+  if (tempAIndex < tempBIndex) {
+    return -1;
+  } else if (tempAIndex > tempBIndex) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function sortMaterials(materialA, materialB) {
+  let materialAIndex = materials.indexOf(materialA.name);
+  let materialBIndex = materials.indexOf(materialB.name);
+
+  if (materialAIndex < materialBIndex) {
+    return -1;
+  } else if (materialAIndex > materialBIndex) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function handleConnectedComponentStudentDataChanged(componentState) {
+  if (componentState.componentType == 'Graph') {
+    //resetTable();
+    const currentMouseOverPointXValue =
+      getCurrentMouseOverPointXValue(componentState);
+
+    // for (let trials of componentState.studentData.trials) {
+    //   addTrialRowToTable(trials.series[0], currentMouseOverPointXValue);
+    // }
+
+    let chartData = generateChartData(componentState.studentData.trials, currentMouseOverPointXValue);
+    categories = chartData.categories;
+    series = chartData.series;
+    lowerRangePoint = chartData.lowerRangePoint;
+    upperRangePoint = chartData.upperRangePoint;
+
+    categories = categories.sort(sortTemperatures);
+    series = series.sort(sortMaterials);
+
+    renderChart(categories, series, lowerRangePoint, upperRangePoint);
+  }
+}
+
+function generateChartData(trials, x) {
+  let categories = [];
+  let series = [];
+  let lowerRangePoint = null;
+  let upperRangePoint = null;
+
+  for (let trial of trials) {
+    if (trial.show) {
+      let trialId = trial.id;
+      let material = getMaterialFromTrialId(trialId);
+      let bevTemp = getBevTempFromTrialId(trialId);
+      let range = getRangeClosestToMouseOverPoint(trial.series[0].data, x);
+      lowerRangePoint = range[0];
+      upperRangePoint = range[1];
+      let changeInTemperature = upperRangePoint.y - lowerRangePoint.y;
+      let color = trial.series[0].color;
+
+      addCategory(categories, bevTemp);
+      addSeries(series, material);
+      let singleSeries = getSeriesByName(series, material);
+      setSeriesValue(singleSeries, bevTemp, changeInTemperature);
+      setSeriesColor(singleSeries, color);
+    }
+  }
+
+  filterTemperatures(categories, series);
+
+
+  return {
+    categories: categories,
+    series: series,
+    lowerRangePoint: lowerRangePoint.x,
+    upperRangePoint: upperRangePoint.x
+  }
+}
+
+
+function filterTemperatures(categories, series) {
+  let hasHot = false;
+  let hasWarm = false;
+  let hasCold = false;
+  for (let singleSeries of series) {
+    if (singleSeries.data[0] != null) {
+      hasHot = true;
+    }
+    if (singleSeries.data[1] != null) {
+      hasWarm = true;
+    }
+    if (singleSeries.data[2] != null) {
+      hasCold = true;
+    }
+  }
+
+  for (let singleSeries of series) {
+    if (!hasCold) {
+      singleSeries.data.splice(2, 1);
+    }
+    if (!hasWarm) {
+      singleSeries.data.splice(1, 1);
+    }
+    if (!hasHot) {
+      singleSeries.data.splice(0, 1);
+    }
+  }
+
+  // if (!hasCold) {
+  //   categories.splice(2, 1);
+  // }
+  // if (!hasWarm) {
+  //   categories.splice(1, 1);
+  // }
+  // if (!hasHot) {
+  //   categories.splice(0, 1);
+  // }
+}
+
+function addCategory(categories, category) {
+  if (!categories.includes(category)) {
+    categories.push(category);
+  }
+}
+
+function addSeries(series, seriesName) {
+  for (let singleSeries of series) {
+    if (singleSeries.name == seriesName) {
+      return;
+    }
+  }
+
+  series.push({ name: seriesName, data: [null, null, null] });
+}
+
+function setSeriesValue(series, category, value) {
+  if (category == 'Hot') {
+    series.data[0] = value;
+  } else if (category == 'Warm') {
+    series.data[1] = value;
+  } else if (category == 'Cold') {
+    series.data[2] = value;
+  }
+}
+
+function setSeriesColor(series, color) {
+  series.color = color;
+}
+
+function getSeriesByName(series, seriesName) {
+  for (let singleSeries of series) {
+    if (singleSeries.name == seriesName) {
+      return singleSeries;
+    }
+  }
+  return null;
+}
+
+function getMaterialFromTrialId(trialId) {
+  let regEx = /(.*)-(.*)Liquid/;
+  let match = regEx.exec(trialId);
+  let material = match[1];
+  return material;
+}
+
+function getBevTempFromTrialId(trialId) {
+  let regEx = /(.*)-(.*)Liquid/;
+  let match = regEx.exec(trialId);
+  let bevTemp = match[2];
+  return bevTemp;
+}
+
+function resetTable() {
+  $("#tempChangeTable").empty();
+  $("#tempChangeTable").append(`<tr><th>Trial ID</th><th>Lower X</th>
+      <th>Upper X</th><th>Change in temperature °C</th><tr/>`);
+}
+
+function addTrialRowToTable(series, currentMouseOverPointXValue) {
+  const range = getRangeClosestToMouseOverPoint(series.data,
+      currentMouseOverPointXValue);
+  const lowerRangePoint = range[0];
+  const upperRangePoint = range[1];
+  //const changeInTemperature = upperRangePoint[1] - lowerRangePoint[1];
+  const changeInTemperature = upperRangePoint.y - lowerRangePoint.y;
+  const changeInTemperatureDesc =
+      getChangeInTemperatureDescription(changeInTemperature);
+  const seriesRow = $(`<tr id="${series.name}">`);
+  seriesRow.append(`<td class="seriesName">${series.name}</td>
+      <td class="lowerY">${lowerRangePoint.x}</td>
+      <td class="upperY">${upperRangePoint.x}</td>
+      <td class="changeInTemperature">
+          ${changeInTemperature.toFixed(2)} (${changeInTemperatureDesc})
+      </td>`);
+  $("#tempChangeTable").append(seriesRow);
+}
+
+function getRangeClosestToMouseOverPoint(seriesData, currentMouseOverPointXValue) {
+  let lowerDataPoint = seriesData[0][0];
+  let upperDataPoint = seriesData[0][0];
+
+  for (let i = 0, j = 1; j < seriesData.length; i++, j++) {
+    lowerDataPoint = seriesData[i];
+    upperDataPoint = seriesData[j];
+    if (lowerDataPoint.x <= currentMouseOverPointXValue &&
+        currentMouseOverPointXValue < upperDataPoint.x) {
+      break;
+    }
+  }
+  return [lowerDataPoint, upperDataPoint];
+}
+
+function getCurrentMouseOverPointXValue(componentState) {
+  let mouseOverPointXValue = 0;
+  if (componentState.studentData.mouseOverPoints != null &&
+      componentState.studentData.mouseOverPoints.length > 0) {
+    let mouseOverPoints = componentState.studentData.mouseOverPoints;
+    mouseOverPointXValue = mouseOverPoints[mouseOverPoints.length - 1][0];
+  }
+  return mouseOverPointXValue;
+}
+
+function getChangeInTemperatureDescription(changeInTemperature) {
+  if (changeInTemperature < 0) {
+    return "cooling";
+  } else if (changeInTemperature > 0) {
+    return "heating";
+  } else {
+    return "same";
+  }
+}
+
+function receiveMessage(message) {
+  if (message != null) {
+    let messageData = message.data;
+    if (messageData != null) {
+      if (messageData.messageType == 'handleConnectedComponentStudentDataChanged') {
+        handleConnectedComponentStudentDataChanged(messageData.componentState);
+      }
+    }
+  }
+}
+
+window.addEventListener('message', receiveMessage);
