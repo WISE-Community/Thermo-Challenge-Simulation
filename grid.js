8b0d1d3bdb6440082b310740ea2f0e62c5e6209a
Hiroki Terashima <honchikun@gmail.com>
2018-12-18 15:27:51 -0800

    Added WISEAPI and changed Grid to extend from it.

--- grid.js
+++ grid.js
@@ -8,7 +8,6 @@ function init() {
   } else if (isFlagMode()) {
     grids = new FlagGrids();
     if (isAutoScoreMaterialMode() || isAutoScoreTemperatureMode()) {
-      initFeedbackButton();
       displayAutoScoreMode();
     }
   } else if (isInterpretMode()) {
@@ -19,7 +18,7 @@ function init() {
   }
   showOnlyAvailableTemps();
   initCellClickedHandlers();
-  sendGetParametersMessage();
+  grids.sendGetParametersMessage();
 }
 
 function showOnlyAvailableTemps() {
@@ -41,8 +40,9 @@ function initCellClickedHandlers() {
   }
 }
 
-class Grids {
+class Grids extends WISEAPI {
   constructor() {
+    super();
     this.completedCells = [];
     this.flaggedCells = [];
     this.selectedCells = [];
@@ -189,27 +189,229 @@ class Grids {
     this.saveToWISE();
   }
 
-  // studentdatachanged on cell clicked
-  // studentwork on submit to get feedback
+  receivedGetParametersMessage(parameters) {
+    this.nodeId = parameters.nodeId;
+    this.componentId = parameters.componentId;
+  }
 
-  saveToWISE() {
-    const componentState = {
-      messageType: 'studentDataChanged',
-      isAutoSave: false,
-      isSubmit: false,
-      timestamp: new Date().getTime(),
-      studentData: {
-        completedCells: this.completedCells,
-        flaggedCells: this.flaggedCells,
-        selectedCells: this.selectedCells
-      },
-      annotations: [{}]
+  handleComponentStateMessage(messageData) {
+    const componentState = messageData.componentState;
+    if (componentState != null) {
+      this.loadComponentState(componentState);
+    }
+  }
+
+  handleConnectedComponentStudentDataChangedMessage(messageData) {
+    const componentState = messageData.componentState;
+    if (componentState.componentType == 'Graph') {
+      showModelStateFromGraphStudentWork(componentState);
+    } else if (componentState.componentType == 'Embedded' &&
+        componentState.studentData.isTrialCompleted) {
+      this.trialCompleted(componentState);
+    }
+  }
+
+  getStudentDataToSave() {
+    return {
+      completedCells: this.completedCells,
+      flaggedCells: this.flaggedCells,
+      selectedCells: this.selectedCells
     };
+  }
+
+  saveFeedbackToWISE(feedback = '') {
+    const studentData = this.getStudentDataToSave();
+    const componentState = this.createComponentState(studentData, 'studentWork');
+    const annotation = this.createAnnotation('autoComment', feedback);
+    this.addAnnotationToComponentState(componentState, annotation);
     try {
-      sendMessageToParent(componentState);
+      this.sendMessageToParent(componentState);
     } catch(err) {
       console.log("not posted");
     }
+  }
+
+  saveToWISE() {
+    const studentData = this.getStudentDataToSave();
+    const componentState = this.createComponentState(studentData, 'studentDataChanged');
+    try {
+      this.sendMessageToParent(componentState);
+    } catch(err) {
+      console.log("not posted");
+    }
+  }
+
+  feedback(feedback) {
+    this.saveFeedbackToWISE(feedback);
+    $("#log").append(feedback);
+    $("#log").append("<br/>");
+    $("#log").append("<br/>");
+  }
+
+  giveFeedback() {
+    const aggregate = this.getFlaggedCellsAggregate();
+    if (this.noCellSelected(aggregate)) {
+      this.feedback("[Condition NoneSelected] Please choose experiment(s) that you want to run.");
+      return;
+    }
+
+    if (this.allCellsSelected(aggregate)) {
+      this.feedback(`[Condition 6Pairs] Are all of these tests necessary? Your plan includes 12 of the 12 possible tests.`);
+      return;
+    }
+
+    if (isAutoScoreTemperatureMode()) {
+      if (this.allHot(aggregate)) {
+        this.feedback("Guidance: all hot");
+      } else if (allCold(aggregate)) {
+        this.feedback("Guidance: all cold");
+      } else if (this.onlyHotAndCold(aggregate)) {
+        this.feedback("Guidance: only hot & cold");
+      } else if (this.hotOnlyEqualsColdOnly(aggregate)) {
+        this.feedback("Guidance: hot only = cold only");
+      } else if (this.hotOnlyMoreThanColdOnly(aggregate)) {
+        this.feedback("Guidance: hot only > cold only");
+      } else if (this.hotOnlyLessThanColdOnly(aggregate)) {
+        this.feedback("Guidance: hot only < cold only");
+      }
+    } else if (isAutoScoreMaterialMode()) {
+      const allMaterialsFlagged = this.getAllMaterialsFlagged(aggregate);
+      if (this.noPairs(aggregate)) {
+        this.feedback(`[Condition NoPairs] It looks like you’re interested in investigating ${allMaterialsFlagged.length} material(s): ${allMaterialsFlagged.join(", ")}.
+          Your plan includes one temperature test for each material.<br/>
+          To improve your experiment plan, consider: 
+          Will testing just one temperature for a material help you decide 
+          how the material compares against other materials for insulating a beverage? `);
+      } else if (this.onlyHotAndCold(aggregate) && this.numPairs(aggregate) === 1) {
+        this.feedback(`[Condition OnePair] It looks like you’re interested in investigating ${allMaterialsFlagged.length} material(s): ${allMaterialsFlagged.join(", ")}.
+          Your plan includes both hot and cold tests for these materials.<br/>
+          To improve your experiment plan, consider: 
+          Are there any other materials you would like to compare to ${allMaterialsFlagged.join(", ")}? 
+          What test(s) should you add to your plan to investigate these other materials?`);
+      } else if (this.onlyHotAndCold(aggregate) && this.numPairs(aggregate) >= 2 && this.numPairs(aggregate) <= 5) {
+        this.feedback(`[Condition 2-5Pairs] It looks like you’re interested in investigating ${allMaterialsFlagged.length} material(s): ${allMaterialsFlagged.join(", ")}.
+          Your plan includes both hot and cold tests for these materials.<br/>
+          To improve your experiment plan, consider: 
+          Are there any other materials you would like to compare to ${allMaterialsFlagged.join(", ")}? 
+          What test(s) should you add to your plan to investigate these other materials?`);
+      } else {
+        this.feedback(`[Condition Singles] It looks like you’re interested in investigating ${allMaterialsFlagged.length} material(s): ${allMaterialsFlagged.join(", ")}.
+          Your plan includes set of hot and cold tests for ${aggregate.hotAndCold.join(", ")} 
+          and only one temperature test each for materials ${aggregate.hotOnly.concat(aggregate.coldOnly).join(", ")}.<br/>
+          To improve your experiment plan, consider: Will testing just one temperature for a material help you decide 
+          how the material compares against other materials for insulating a beverage?`);
+      }
+    }
+  }
+
+  getFlaggedCellsAggregate() {
+    const flaggedCellsByMaterial = {};
+    const flaggedCellsHotAndCold = [];
+    const flaggedCellsHotOnly = [];
+    const flaggedCellsColdOnly = [];
+    ["Aluminum", "Wood", "Styrofoam", "Clay", "Glass", "Plastic"].map((material) => {
+      flaggedCellsByMaterial[material] = this.getFlaggedCellsByMaterial(material);
+      const hotMaterial = this.getFlaggedCellsByMaterialAndTemp(material, "Hot");
+      const coldMaterial = this.getFlaggedCellsByMaterialAndTemp(material, "Cold");
+      if (hotMaterial.length === 1 && coldMaterial.length === 1) {
+        flaggedCellsHotAndCold.push(material);
+      } else if (hotMaterial.length === 1) {
+        flaggedCellsHotOnly.push(material);
+      } else if (coldMaterial.length === 1) {
+        flaggedCellsColdOnly.push(material);
+      }
+    });
+    const flaggedCellsByTemp = {};
+    ["Hot", "Cold"].map((temp) => {
+      flaggedCellsByTemp[temp] = this.getFlaggedCellsByTemp(temp);
+    });
+    return {
+      byMaterial: flaggedCellsByMaterial,
+      byTemp: flaggedCellsByTemp,
+      hotAndCold: flaggedCellsHotAndCold,
+      hotOnly: flaggedCellsHotOnly,
+      coldOnly: flaggedCellsColdOnly
+    };
+  }
+
+  noCellSelected(aggregate) {
+    return aggregate.hotOnly.length === 0 &&
+      aggregate.coldOnly.length === 0 &&
+      aggregate.hotAndCold.length === 0;
+  }
+
+  allCellsSelected(aggregate) {
+    return aggregate.hotAndCold.length === materials.length;
+  }
+
+  noPairs(aggregate) {
+    return aggregate.hotAndCold.length === 0 &&
+      (aggregate.hotOnly.length > 0 || aggregate.coldOnly.length > 0);
+  }
+
+  getAllMaterialsFlagged(aggregate) {
+    return aggregate.hotAndCold.concat(aggregate.hotOnly).concat(aggregate.coldOnly);
+  }
+
+  numPairs(aggregate) {
+    return aggregate.hotAndCold.length;
+  }
+
+  allHot(aggregate) {
+    return aggregate.hotAndCold.length + aggregate.hotOnly.length === materials.length;
+  }
+
+  allCold(aggregate) {
+    return aggregate.hotAndCold.length + aggregate.coldOnly.length === materials.length;
+  }
+
+  onlyHotAndCold(aggregate) {
+    return aggregate.hotAndCold.length > 0 &&
+      aggregate.hotOnly.length === 0 && aggregate.coldOnly.length === 0;
+  }
+
+  hotOnlyEqualsColdOnly(aggregate) {
+    return aggregate.hotOnly.length > 0 &&
+      aggregate.hotOnly.length === aggregate.coldOnly.length;
+  }
+
+  hotOnlyMoreThanColdOnly(aggregate) {
+    return aggregate.hotOnly.length > 0 &&
+      aggregate.hotOnly.length > aggregate.coldOnly.length;
+  }
+
+  hotOnlyLessThanColdOnly(aggregate) {
+    return aggregate.coldOnly.length > 0 &&
+      aggregate.hotOnly.length < aggregate.coldOnly.length;
+  }
+
+  getFlaggedCellsByMaterial(material) {
+    return this.flaggedCells.filter((flaggedCell) => {
+      return flaggedCell.material === material;
+    });
+  }
+
+  getFlaggedCellsByTemp(temp) {
+    return this.flaggedCells.filter((flaggedCell) => {
+      return flaggedCell.bevTemp === temp;
+    });
+  }
+
+  getFlaggedCellsByMaterialAndTemp(material, temp) {
+    return this.flaggedCells.filter((flaggedCell) => {
+      return flaggedCell.material === material && flaggedCell.bevTemp === temp;
+    });
+  }
+
+  clearLog() {
+    $("#log").html("");
+  }
+
+  appendStateToLog(aggregate) {
+    $("#log").append(`Number of tests: ${aggregate.hotAndCold.length * 2 + aggregate.hotOnly.length + aggregate.coldOnly.length} / 12`);
+    $("#log").append(`Hot and Cold (${aggregate.hotAndCold.length}): [${aggregate.hotAndCold.join(",")}]`);
+    $("#log").append(`Hot only (${aggregate.hotOnly.length}): [${aggregate.hotOnly.join(",")}]`);
+    $("#log").append(`Cold only (${aggregate.coldOnly.length}): [${aggregate.coldOnly.join(",")}]`);
   }
 }
 
@@ -303,94 +505,7 @@ class InterpretGrids extends Grids {
   }
 }
 
-function initFeedbackButton() {
-  $("#feedbackButton").show();
-  $("#feedbackButton").click(() => {
-    giveGuidance();
-  });
-}
-
 function displayAutoScoreMode() {
+  $("#autoScoreDiv").show();
   $("#autoScoreGuidanceHeading").append(` (${getAutoScoreMode()})`);
 }
-
-function sendGetParametersMessage() {
-  sendMessageToParent({ messageType: 'getParameters' });
-}
-
-function receivedGetParametersMessage(parameters) {
-  grids.nodeId = parameters.nodeId;
-  grids.componentId = parameters.componentId;
-}
-
-function sendApplicationInitializedMessage() {
-  sendMessageToParent({ messageType: 'applicationInitialized' });
-}
-
-function loadComponentState(componentState) {
-  grids.loadComponentState(componentState);
-}
-
-/**
- * Send a message to the parent
- * @param the message to send to the parent
- */
-function sendMessageToParent(message) {
-  window.postMessage(message, "*");
-}
-
-/**
- * Receive a message from the parent
- * @param message the message from the parent
- */
-function receiveMessageFromParent(message) {
-  if (message != null) {
-    let messageData = message.data;
-    if (messageData != null) {
-      if (messageData.messageType == 'studentWork') {
-        /*
-         * we have received a message that contains student work from
-         * other components
-         */
-        this.studentWorkFromThisNode = messageData.studentWorkFromThisNode;
-        this.studentWorkFromOtherComponents = messageData.studentWorkFromOtherComponents;
-
-      } else if (messageData.messageType == 'nodeSubmitClicked') {
-        /*
-         * the student has clicked the submit button and the student
-         * work has been included in the message data
-         */
-        this.studentWorkFromThisNode = messageData.studentWorkFromThisNode;
-        this.studentWorkFromOtherComponents = messageData.studentWorkFromOtherComponents;
-      } else if (messageData.messageType == 'componentStateSaved') {
-        let componentState = messageData.componentState;
-      } else if (messageData.messageType == 'parameters') {
-        // WISE has sent the parameters to us
-        //console.log(messageData.parameters);
-        receivedGetParametersMessage(messageData.parameters);
-        sendApplicationInitializedMessage();
-      } else if (messageData.messageType == 'siblingComponentStudentDataChanged') {
-        let componentState = messageData.componentState;
-      } else if (messageData.messageType == 'handleConnectedComponentStudentDataChanged') {
-        let componentState = messageData.componentState;
-        if (componentState.componentType == 'Graph') {
-          showModelStateFromGraphStudentWork(componentState);
-        } else if (messageData.messageType == 'handleConnectedComponentStudentDataChanged') {
-          let componentState = messageData.componentState;
-          if (componentState.componentType == 'Embedded' &&
-            componentState.studentData.isTrialCompleted) {
-            grids.trialCompleted(componentState);
-          }
-        }
-      } else if (messageData.messageType == 'componentState') {
-        let componentState = messageData.componentState;
-        if (componentState != null) {
-          loadComponentState(componentState);
-        }
-      }
-    }
-  }
-}
-
-// listen for messages from the parent frame
-window.addEventListener('message', receiveMessageFromParent);
